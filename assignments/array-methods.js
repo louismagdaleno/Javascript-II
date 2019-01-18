76e7381428afeced2cad4281e855e545aa697ceb
Louis Magdaleno <buildthatapp@gmail.com>
2019-01-18 15:02:21 -0800

    Completed callback.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -85,7 +85,13 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Find total number of participants by shirt size and log the result
+let runnersByShirtSize = [];
 
+
+
+
+console.log(runnersByShirtSize);
 // Problem 2
 
 // Problem 3
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -21,26 +21,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+function secondItem(arr, cb) {
+  return cb(arr[1]);
+}
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      return cb(arr[i]);
+    }
+  }
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+
+  return cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let product = x * y;
+
+  return cb(product);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.forEach(x => {
+    if (x === item) {
+      return cb(true);
+    }
+    else {
+      return cb(false);
+    }
+  })
 }
 
 /* STRETCH PROBLEM */
@@ -49,4 +73,16 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+  let duplicateFree = [];
+
+  // Use a Set to include unique values. If not set, 
+  // iterate over array and push to duplicateFree if duplicateFree does not already include it
+  duplicateFree = new Set(array);
+
+  return cb(duplicateFree);
 }
+
+
+
+
