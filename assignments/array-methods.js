b6379299e825ef60bdd5791cc446e563772e5799
Louis Magdaleno <buildthatapp@gmail.com>
2019-01-21 22:21:10 -0800

    Reworked Solutions in array-methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -68,48 +68,54 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
-largeShirts = runners.filter(runner => { 
-    if ( runner.shirt_size === 'L') {
-        return runner;
-    }
-});
-console.log(JSON.stringify(largeShirts));
+largeShirts = runners.filter(runner => runner.shirt_size === 'L');
+console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-ticketPriceTotal = runners.reduce((previous, current) => previous + current.donation, 0);
+ticketPriceTotal = runners.reduce((total, runner) => runner.donation + total, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-// Print a list of unique company names, sorted ascending, that are participating in the run.
+// Find the total number of donations made by people wearing large shirts
+let largeShirtDonations = [];
 
-let uniqueCompanies = [];
+largeShirtDonations = 
+    runners.filter(runner => runner.shirt_size === 'L')
+    .map(runner => runner.donation)
+    .reduce((total, current) => total + current, 0);
 
-uniqueCompanies = new Set(runners.map(runner => runner.company_name).sort());
-
-console.log(uniqueCompanies);
-
-
+console.log(largeShirtDonations);
 
 // Problem 2
-// The company wants a list of emails of the participants. create a list of email addresses and log it to the console.
-
-let email_addresses = [];
-
-runners.forEach(runner => email_addresses.push(runner.email));
-
-console.log(email_addresses);
+// Display a list of email addresses of participants who wore size M shirts and donated 50 or greater.
+let highRollers = [];
+highRollers = 
+    runners.filter(runner => runner.shirt_size === 'M' && runner.donation >= 50)
+    .map(runner => runner.email);
+    
+console.log(highRollers);
 
 // Problem 3
-// The company Skinix has requested a list of their employees who participated. Store that information in an array skinix_employees
-// and log the data to the console.
+// Find the total donations by shirt sizes and display the results
 
-let skinix_employees = [];
+let totalShirtDonations = [];
 
-skinix_employees = runners.filter(runner => runner.company_name === 'Skinix');
+// create unique list of shirt sizes and an iterator
+let sizes = new Set(runners.map(runner => runner.shirt_size));
+let iterator = sizes.values();
 
-console.log(JSON.stringify(skinix_employees));
+
+sizes.forEach(item => totalShirtDonations.push( 
+    runners.filter(runner => runner.shirt_size === item)
+    .map(runner => runner.donation)
+    .reduce((total, current) => total + current, 0)
+    ));
+  
+for (let i = 0; i < sizes.size; i++) {
+    console.log(`${iterator.next().value} : ${totalShirtDonations[i]}`);
+}
